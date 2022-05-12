2e06f44f19f325158ccfc987e8830e0831a6d665
Christian Weber <webdog@users.noreply.github.com>
2022-05-12 12:25:20 -0500

    relieve dependency pressure

--- providers.tf
+++ providers.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     aws = {
         source = "hashicorp/aws"
-        version = "3.66.0"
+        version = ">=3.66.0"
     }
     null = {
         source = "hashicorp/null"
-        version = "3.1.1"
+        version = ">=3.1.1"
     }
   }
 }
